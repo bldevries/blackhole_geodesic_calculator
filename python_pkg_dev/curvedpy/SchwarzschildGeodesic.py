FIxed bugs with normalization. Refactored some code.

python_pkg_dev/curvedpy/SchwarzschildGeodesic.py
@@ -8,6 +8,7 @@ class SchwarzschildGeodesic:
     
 
     def __init__(self, time_like = False, r_s_value = 1):
+
         self.r_s_value = r_s_value
         self.time_like = time_like
 
@@ -134,40 +135,36 @@ class SchwarzschildGeodesic:
         return result
 
 
-    def ray_trace(  self, direction, loc_bh, loc_hit, \
-                    R_obj_blender=1, \
+    def ray_trace(  self, direction, loc_hit, \
                     ratio_obj_to_blackhole = 20, \
                     exit_tolerance = 0.1, \
                     curve_end = -1, \
                     warnings = True, verbose=False):
+        # loc_hit: the BH is assumed to be located at the origin and loc_hit is relative to the origin and thus location of the BH
         # R_obj_blender: the size of the object representing the black hole in Blender
         # exit_tolerance: the ray tracing stops when it exits the sphere of influence. You can change the size of the 
         # sphere a bit to determine where it exits. This is done using: x**2 + y**2 + z**2 < (R_influence*(exit_tolerance+1.0))**2
 
-        R_influence = self.r_s_value*ratio_obj_to_blackhole #10 # This is the dinemsion sphere of influence of the BH
-        
-        if verbose:
-            if R_obj_blender != 1: print("R_obj_blender: THIS IS NOT IMPLEMENTED")
-            
-        direction = direction/np.linalg.norm(direction)
-        if verbose: print("loc_hit", loc_hit)
-        loc_hit = np.array(loc_hit) - np.array(loc_bh)
-        if verbose: print("loc_hit-loc_bh", loc_hit)
-        loc_hit = loc_hit * R_influence # THIS NEEDS TO CHANGE IF R_obj_blender IS IMPLEMENTED
-        if verbose: print("(loc_hit-loc_bh)*R_infl", loc_hit)
 
-        if not np.linalg.norm(loc_hit) == 10.: 
-            if warnings:
-                print("Strange hit location not on sphere: norm(hit)", np.linalg.norm(loc_hit))
+        direction = direction/np.linalg.norm(direction) # Normalize the direction of the photon
+        loc_hit_original = loc_hit
+
+        #loc_hit = np.array(loc_hit) # - np.array(loc_bh) # Get the hit coords relative to the BH
+        #loc_hit_original_relBH = loc_hit
+
+        R_sphere = np.linalg.norm(loc_hit) # The size of the sphere in Blender
+        R_schwarz = R_sphere / ratio_obj_to_blackhole # The size of the BH in Blender
+
+        scale_factor = 1/R_schwarz # Everything gets scaled by this factor to have Schwarzschild radius of 1
+
+        loc_hit = loc_hit * scale_factor
+        R_sphere_scaled = R_sphere * scale_factor
         
         # Here I scale curve_end because otherwise, with a large R_influence, 
         # the integrator does not reach the otherside of the sphere
         # nr_points_curve SCHALING DIT MOET BETER!
         if curve_end == -1:
-            curve_end = int(50*R_influence/10.)
-        # if nr_points_curve == -1:
-        #     nr_points_curve = int(50*R_influence/10.)
-
+            curve_end = int(50*R_sphere_scaled)#/10.)
 
         res = self.calc_trajectory(\
                         k_x_0 = direction[0], k_y_0 = direction[1], k_z_0 = direction[2], \
@@ -180,18 +177,20 @@ class SchwarzschildGeodesic:
         if verbose: print(res)
         if verbose: print("Start before cut: ", x[0], y[0], z[0])
         
-      
+        x = x/scale_factor #+ loc_bh[0]
+        y = y/scale_factor #+ loc_bh[1]
+        z = z/scale_factor #+ loc_bh[2]
+
         list_i = []
-        if verbose: print("exit", R_influence, exit_tolerance)
         for i in range(len(x)):
-            if x[i]**2 + y[i]**2 + z[i]**2 < (R_influence*(exit_tolerance+1.0))**2:
-                #print("reached it")
-                #print(x[i]**2 + y[i]**2 + z[i]**2, R_influence**2, i, len(x))
+            if x[i]**2 + y[i]**2 + z[i]**2 < (R_sphere*(exit_tolerance+1.0))**2:
                 list_i.append(i)
+
+        list_i.append(list_i[-1]+1) # Add one more element outside
 
         if verbose: print("Start after cut: ", x[0], y[0], z[0])
 
-        if len(list_i) == 0:
+        if len(list_i) == 0 or res["hit_blackhole"]:
             return x, y, z, [], []
         else:
             x = x[list_i]
@@ -201,74 +200,10 @@ class SchwarzschildGeodesic:
             k_y = k_y[list_i]
             k_z = k_z[list_i]
 
-            
-            x, y, z = x/R_influence + loc_bh[0], y/R_influence + loc_bh[1], z/R_influence + loc_bh[2]
-            end_loc, end_dir = np.array([x[-1], y[-1], z[-1]]), np.array([k_x[-1], k_y[-1], k_z[-1]]) 
+            # Forced normalization on the end_dir since it gave errors in trig functions. But need to
+            # see how much the direction from the integrator deviates from normalized.
+            end_dir = np.array([k_x[-1], k_y[-1], k_z[-1]]) / np.linalg.norm(np.array([k_x[-1], k_y[-1], k_z[-1]]))
+            end_loc = np.array([x[-1], y[-1], z[-1]])
 
             if verbose: print("Start after cut and rescaling: ", x[0], y[0], z[0])
             return x, y, z, end_loc, end_dir
-
-
-
-    # # Grid is not needed :)
-    # def calc_grid(self, \
-    #               affine_param_start = 0, affine_param_end = 50, affine_param_nr_points = 100, \
-    #               x0 = -10, y0_start = 0, y0_end = 30.1, z0_start = 0, z0_end = 30.1, \
-    #               y_spacing = 1.0, z_spacing = 1.0, \
-    #               k_x_0 = 1., k_y_0 = 0., k_z_0 = 0.):
-    #     #grid_x0 = np.array([-10])
-    #     #x0 = -10.
-    #     grid_y0 = np.arange(y0_start, y0_end, y_spacing)
-    #     grid_z0 = np.arange(z0_start, z0_end, z_spacing)
-
-    #     print("Expected compute: ", len(grid_y0)*len(grid_z0)*0.003, "sec, for ", \
-    #           len(grid_y0)*len(grid_z0), " models")
-    #     print("x0: ", x0)
-    #     print("y0 range: ", min(grid_y0), max(grid_y0))
-    #     print("z0 range: ", min(grid_z0), max(grid_z0))
-        
-    #     start = time.time()
-    #     results = [[{"x0": x0, "y0": y0, "z0": z0, "result":\
-    #                  self.calc_trajectory(\
-    #                     k_x_0 = k_x_0, k_y_0 = k_y_0, k_z_0 = k_z_0, \
-    #                     x0 = x0, y0 = y0, z0 = z0, \
-    #                     curve_start = affine_param_start, \
-    #                     curve_end = affine_param_end, \
-    #                     nr_points_curve = affine_param_nr_points, \
-    #                     verbose = False \
-    #                    )\
-    #                 } for z0 in grid_z0] for y0 in grid_y0]
-    #     end = time.time()
-    #     print(end-start)
-        
-    #     return results
-
-    # def calc_1d_grid(self, \
-    #               affine_param_start = 0, affine_param_end = 50, affine_param_nr_points = 100, \
-    #               x0 = -10, y0_start = 0, y0_end = 30.1, z0 = 0, \
-    #               y_spacing = 1.0, \
-    #               k_x_0 = 1., k_y_0 = 0., k_z_0 = 0.):
-
-    #     grid_y0 = np.arange(y0_start, y0_end, y_spacing)
-
-    #     print("Expected compute: ", len(grid_y0)*0.003, "sec, for ", \
-    #           len(grid_y0), " models")
-    #     print("x0: ", x0)
-    #     print("y0 range: ", min(grid_y0), max(grid_y0))
-    #     print("z0: ", z0)
-        
-    #     start = time.time()
-    #     results = [{"x0": x0, "y0": y0, "z0": z0, "result":\
-    #                  self.calc_trajectory(\
-    #                     k_x_0 = k_x_0, k_y_0 = k_y_0, k_z_0 = k_z_0, \
-    #                     x0 = x0, y0 = y0, z0 = z0, \
-    #                     curve_start = affine_param_start, \
-    #                     curve_end = affine_param_end, \
-    #                     nr_points_curve = affine_param_nr_points, \
-    #                     verbose = False \
-    #                    )\
-    #                 } for y0 in grid_y0]
-    #     end = time.time()
-    #     print(end-start)
-        
-    #     return results